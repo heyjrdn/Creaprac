4bec07174fcac8d073d3f79cbf578d2ba5d17465
Jordan Cortes Guzman <jordancg91@gmail.com>
2013-12-20 01:50:34 -0600

    Se agregaron las páginas de respuestas del correo fixes #2

--- php/PHPMailer/mail.php
+++ php/PHPMailer/mail.php
@@ -15,42 +15,49 @@ $emailTo    = 'jordancg91@gmail.com';
 $nameTo     = 'Jordan Cortes';
 
 //Form Data
-$name       =  (string) htmlspecialchars( $_POST['first'] );
-$email      =  (string) htmlspecialchars( $_POST['email'] );
-$phone      =  (string) htmlspecialchars( $_POST['phone'] );
-$company    =  (string) htmlspecialchars( $_POST['company'] );
-$city       =  (string) htmlspecialchars( $_POST['city'] );
-$state      =  (string) htmlspecialchars( $_POST['state'] );
-$country    =  (string) htmlspecialchars( $_POST['country'] );
-$message    =  (string) htmlspecialchars( $_POST['message'] );
+$name       =  (string) htmlentities( $_POST['first'] );
+$email      =  (string) htmlentities( $_POST['email'] );
+$phone      =  (string) htmlentities( $_POST['phone'] );
+$company    =  (string) htmlentities( $_POST['company'] );
+$city       =  (string) htmlentities( $_POST['city'] );
+$state      =  (string) htmlentities( $_POST['state'] );
+$country    =  (string) htmlentities( $_POST['country'] );
+$message    =  (string) htmlentities( $_POST['message'] );
 
-$mail->isSMTP();
-$mail->Host = 'smtp.creaprac.com';
-$mail->SMTPAuth = true;
-$mail->Port = 587;
-$mail->Username = $Username;
-$mail->Password = $Password;
+if ($name != '' && $email != '' && $phone != '' && $company != '' && $city != '' && $state != '' && $country != '' && $message != '')
+{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.creaprac.com';
+    $mail->SMTPAuth = true;
+    $mail->Port = 587;
+    $mail->Username = $Username;
+    $mail->Password = $Password;
 
-$mail->From = $Username;
-$mail->FromName = $FromName;
-$mail->addAddress($emailTo, $nameTo);
-$mail->addReplyTo($email, $name);
+    $mail->From = $Username;
+    $mail->FromName = $FromName;
+    $mail->addAddress($emailTo, $nameTo);
+    $mail->addReplyTo($email, $name);
 
-$mail->WordWrap = 50;
-$mail->isHTML(true);
+    $mail->WordWrap = 50;
+    $mail->isHTML(true);
 
-$mail->Subject = 'Feedback via Creaprac.com';
-$mail->Body    = bodyHTML($name, $email, $phone, $company, $city, $state, $country, nl2br( $message ));
-$mail->AltBody = bodyPlain($name, $email, $phone, $company, $city, $state, $country, $message);
+    $mail->Subject = 'Feedback via Creaprac.com';
+    $mail->Body    = bodyHTML($name, $email, $phone, $company, $city, $state, $country, nl2br( $message ));
+    $mail->AltBody = bodyPlain($name, $email, $phone, $company, $city, $state, $country, $message);
+}
 
 if(!$mail->send()) {
-   echo 'Message could not be sent.';
-   echo 'Mailer Error: ' . $mail->ErrorInfo;
+    header("Location: http://demo.creaprac.com/MX/contact/error.html");
+    die();
+   //echo 'El mensaje no se pudo enviar.';
+   //echo 'Mailer Error: ' . $mail->ErrorInfo;
    exit;
 }
 
-echo $message . "\n.";
+//echo $message . "\n.";
+//echo 'Message has been sent';
 
-echo 'Message has been sent';
+header("Location: http://demo.creaprac.com/MX/contact/success.html");
+die();
 
 ?>
